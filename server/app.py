9902dfab43011a6ce92285ae92635e866d01ed53
Sabrina Freifeld <sfrei.1996@gmail.com>
2024-04-30 07:44:52 -0600

    code challenge

--- server/app.py
+++ server/app.py
@@ -25,5 +25,76 @@ def index():
     return "<h1>Code challenge</h1>"
 
 
+@app.route('/restaurants', methods=["GET"])
+def all_restaurants():
+    all_restaurants = Restaurant.query.all()
+    r_l = []
+    for restaurant in all_restaurants:
+        r_l.append(restaurant.to_dict(rules = ("-restaurant_pizzas",)))
+    return r_l
+
+@app.route('/restaurants/<int:id>', methods=["GET", "DELETE"])
+def one_restaurant(id):
+    one_restaurant = Restaurant.query.filter(Restaurant.id == id).first()
+    if one_restaurant:
+        if request.method == "GET":
+            return one_restaurant.to_dict(),200
+        elif request.method == "DELETE":
+            db.session.delete(one_restaurant)
+            db.session.commit()
+            return {},204
+    else:
+        return {"error": "Restaurant not found"},404
+    
+
+@app.route('/pizzas', methods=["GET"])
+def all_pizzas():
+    all_pizzas = Pizza.query.all()
+    r_l = []
+    for pizza in all_pizzas:
+        r_l.append(pizza.to_dict(rules = ("-restaurant_pizzas",)))
+    return r_l
+
+@app.route('/restaurant_pizzas', methods=["POST"])
+def all_restaurant_pizzas():
+    try:
+        data = request.get_json()
+        rp = RestaurantPizza(
+            price = data['price'],
+            pizza_id = data['pizza_id'],
+            restaurant_id = data['restaurant_id']
+        )
+        db.session.add(rp)
+        db.session.commit()
+        
+        new_pizza = RestaurantPizza.query.filter_by(id=rp.id).first()
+
+        response_data = {
+                "id": new_pizza.id,
+                "price": new_pizza.price,
+                "pizza": {
+                    "id": new_pizza.pizza.id,
+                    "name": new_pizza.pizza.name,
+                    "ingredients": new_pizza.pizza.ingredients
+                },
+                "pizza_id": new_pizza.pizza_id,
+                "restaurant": {
+                    "id": new_pizza.restaurant.id,
+                    "name": new_pizza.restaurant.name,
+                    "address": new_pizza.restaurant.address
+                },
+                "restaurant_id": new_pizza.restaurant_id
+            }
+
+
+        return make_response(response_data, 201)
+
+    except ValueError:
+        return {"errors": ["validation errors"]},400
+
+
+
+
+
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
-from sqlalchemy.orm import validates
+from sqlalchemy.orm import validates, relationship
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
@@ -21,8 +21,9 @@ class Restaurant(db.Model, SerializerMixin):
     address = db.Column(db.String)
 
     # add relationship
-
+    restaurant_pizzas = relationship("RestaurantPizza", cascade='delete,all', back_populates="restaurant")
     # add serialization rules
+    serialize_rules = ('-restaurant_pizzas.restaurant',)
 
     def __repr__(self):
         return f"<Restaurant {self.name}>"
@@ -36,8 +37,11 @@ class Pizza(db.Model, SerializerMixin):
     ingredients = db.Column(db.String)
 
     # add relationship
+    restaurant_pizzas = relationship("RestaurantPizza", cascade='delete,all', back_populates="pizza")
+    
 
     # add serialization rules
+    serialize_rules = ('-restaurant_pizzas.pizza',)
 
     def __repr__(self):
         return f"<Pizza {self.name}, {self.ingredients}>"
@@ -50,10 +54,22 @@ class RestaurantPizza(db.Model, SerializerMixin):
     price = db.Column(db.Integer, nullable=False)
 
     # add relationships
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
 
-    # add serialization rules
+    restaurant = relationship("Restaurant", back_populates="restaurant_pizzas")
+    pizza = relationship("Pizza", back_populates="restaurant_pizzas")
+
+    serialize_rules = ('-restaurant', '-pizza')
 
     # add validation
+    @validates('price')
+    def validate_price(self,key,value):
+        if 1 <= value <= 30:
+            return value
+        else:
+            raise ValueError(f"Price must be between 1 and 30 {key}")
+
 
     def __repr__(self):
         return f"<RestaurantPizza ${self.price}>"
